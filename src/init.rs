a1bdb2ab14eddbf29b92da2a5893539c792df809
fujimori25 <tfujimori@fab.pasona.tech>
2025-09-02 08:53:51 +0900

    ~292コード整理

--- src/init.rs
+++ src/init.rs
@@ -7,6 +7,7 @@ use crate::hpet::Hpet;
 use crate::info;
 use crate::uefi::exit_from_efi_boot_services;
 use crate::uefi::EfiHandle;
+use crate::uefi::EfiMemoryType;
 use crate::uefi::EfiMemoryType::*;
 use crate::uefi::EfiSystemTable;
 use crate::uefi::MemoryMapHolder;
@@ -44,6 +45,10 @@ pub fn init_paging(memory_map: &MemoryMapHolder) {
     table
         .create_mapping(0, end_of_mem, 0, PageAttr::ReadWriteKernel)
         .expect("Failed to create initial page mapping");
+    // Unmap page 0 to detect null ptr dereference
+    table
+        .create_mapping(0, 4096, 0, PageAttr::NotPresent)
+        .expect("Failed to unmap page 0");
     unsafe {
         write_cr3(Box::into_raw(table));
     }
@@ -57,4 +62,17 @@ pub fn init_hpet(acpi: &AcpiRsdpStruct) {
     info!("HPET is at {hpet:#p}");
     let hpet = Hpet::new(hpet);
     set_global_hpet(hpet);
+}
+
+pub fn init_allocator(memory_map: &MemoryMapHolder) {
+    let mut total_memory_pages = 0;
+    for e in memory_map.iter() {
+        if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
+            continue;
+        }
+        total_memory_pages += e.number_of_pages();
+        info!("{e:?}");
+    }
+    let total_memory_size_mib = total_memory_pages * 4096 / 1024 / 1024;
+    info!("Total : {total_memory_pages} pages = {total_memory_size_mib} MiB");
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use core::panic::PanicInfo;
 use core::time::Duration;
 use core::writeln;
 use wasabi::error;
-use wasabi::executor;
 use wasabi::executor::Executor;
 use wasabi::executor::Task;
 use wasabi::executor::TimeoutFuture;
@@ -16,27 +15,22 @@ use wasabi::graphics::draw_test_pattern;
 use wasabi::graphics::fill_rect;
 use wasabi::graphics::Bitmap;
 use wasabi::hpet::global_timestamp;
+use wasabi::info;
+use wasabi::init::init_allocator;
 use wasabi::init::init_basic_runtime;
 use wasabi::init::init_hpet;
 use wasabi::init::init_paging;
 use wasabi::print::hexdump;
 use wasabi::println;
-use wasabi::info;
 use wasabi::warn;
 use wasabi::qemu::exit_qemu;
 use wasabi::qemu::QemuExitCode;
 use wasabi::uefi::init_vram;
 use wasabi::uefi::locate_loaded_image_protocol;
 use wasabi::uefi::EfiHandle;
-use wasabi::uefi::EfiMemoryType;
 use wasabi::uefi::EfiSystemTable;
 use wasabi::uefi::VramTextWriter;
-use wasabi::x86::flush_tlb;
-use wasabi::x86::hlt;
 use wasabi::x86::init_exceptions;
-use wasabi::x86::read_cr3;
-use wasabi::x86::trigger_debug_interrupt;
-use wasabi::x86::PageAttr;
 
 #[no_mangle]
 fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
@@ -61,44 +55,11 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let acpi = efi_system_table.acpi_table().expect("ACPI table not found");
 
     let memory_map = init_basic_runtime(image_handle, efi_system_table);
-    let mut total_memory_pages = 0;
-    for e in memory_map.iter() {
-        if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
-            continue;
-        }
-        total_memory_pages += e.number_of_pages();
-        writeln!(w, "{e:?}").unwrap();
-    }
-    let total_memory_size_mib = total_memory_pages * 4096 / 1024 / 1024;
-    writeln!(w, "Total: {total_memory_pages} pages = {total_memory_size_mib} MiB").unwrap();
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
-    let cr3 = wasabi::x86::read_cr3();
-    println!("cr3 = {cr3:#p}");
-    let t = Some(unsafe { &*cr3 });
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
+    init_allocator(&memory_map);
 
     let (_gdt, _idt) = init_exceptions();
-    info!("Exception initialized!");
-    trigger_debug_interrupt();
-    info!("Execution continued");
     init_paging(&memory_map);
-    info!("Now we are using our own page tables!");
-
-    // Unmap page 0 to detect null ptr dereference
-    let page_table = read_cr3();
-    unsafe {
-        (*page_table)
-            .create_mapping(0, 4096, 0, PageAttr::NotPresent)
-            .expect("Failed to unmap page 0");
-    }
-    flush_tlb();
-
     init_hpet(acpi);
     let t0 = global_timestamp();
     let task1 = Task::new(async move {
