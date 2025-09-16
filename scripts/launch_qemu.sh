c10f03596af5ddb5b968f247e5eafcd70b7542e5
fujimori25 <tfujimori@fab.pasona.tech>
2025-09-17 07:42:43 +0900

    Enable qemu-xhci

--- scripts/launch_qemu.sh
+++ scripts/launch_qemu.sh
@@ -16,6 +16,7 @@ qemu-system-x86_64 \
     -monitor telnet:0.0.0.0:2345,server,nowait,logfile=log/qemu_monitor.txt \
     -chardev stdio,id=char_com1,mux=on,logfile=log/com1.text \
     -serial chardev:char_com1 \
+    -device qemu-xhci \
     -device isa-debug-exit,iobase=0xf4,iosize=0x01
 RETCODE=$?
 set -e
