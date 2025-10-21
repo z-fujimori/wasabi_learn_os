a62e7914fbd381f7036a8c165b85cf3d30054f38
fujimori25 <tfujimori@fab.pasona.tech>
2025-10-21 20:56:56 +0900

    xHCの存在を確認

--- src/pci.rs
+++ src/pci.rs
@@ -1,6 +1,7 @@
 use crate::acpi::AcpiMcfgDescriptor;
 use crate::info;
 use crate::result::Result;
+use crate::xhci::PciXhciDriver;
 use core::fmt;
 use core::marker::PhantomData;
 use core::mem::size_of;
@@ -164,6 +165,9 @@ impl Pci {
         for bdf in BusDeviceFunction::iter() {
             if let Some(vd) = self.read_vendor_id_and_device_id(bdf) {
                 info!("{vd}");
+                if PciXhciDriver::supports(vd) {
+                    PciXhciDriver::attach(bdf);
+                }
             }
         }
     }
