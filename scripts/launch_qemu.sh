718910c06a935bbf10c92558f9171a53e24a4894
fujimori25 <tfujimori@fab.pasona.tech>
2025-11-10 22:29:19 +0900

    タッチパネルを繋げる

--- scripts/launch_qemu.sh
+++ scripts/launch_qemu.sh
@@ -18,6 +18,7 @@ qemu-system-x86_64 \
     -serial chardev:char_com1 \
     -device qemu-xhci \
     -device usb-kbd \
+    -device usb-tablet \
     -device isa-debug-exit,iobase=0xf4,iosize=0x01
 RETCODE=$?
 set -e
